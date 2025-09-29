1401a89d379a21ef4de0e3495ebc25bdb980ee97
mahima-saini <141727847+mahima-saini@users.noreply.github.com>
2025-09-29 15:38:45 +0530

    add endpoint

--- src/services/homeService.jsx
+++ src/services/homeService.jsx
@@ -3,7 +3,7 @@ import endPoint from "./axios/apiEndpoint";
 
 export const fetchQuestions = async (setLoading) => {
   try {
-    const response = await getData(endPoint.ALL_QUESTIONS);
+    const response = await getData(endPoint.ASK_IBU_QUESTIONS);
     return response?.data?.data;
   } catch (error) {
     console.error("Error fetching Ask IBU questions:", error);
@@ -24,7 +24,7 @@ export const handleSubmit = async (e, setError, question, setQuestion) => {
 
   setError("");
   try {
-    const response = await postData("/api/ibu/add-ibu-question", {
+    const response = await postData(endPoint.ADD_QUESTIONS, {
       question: question,
     });
 
@@ -36,7 +36,7 @@ export const handleSubmit = async (e, setError, question, setQuestion) => {
 
 export const fetchTags = async () => {
   try {
-    const response = await getData("/api/content/categories-age-groups");
+    const response = await getData(endPoint.GET_AGE_GROUP_CATEGORIES);
     return response?.data?.data?.tags || [];
   } catch (error) {
     console.error("Error fetching tags:", error);
