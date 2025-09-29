def20c83c252b0d03710328be49074a398b09081
unknown <shivam.chauhan@shinedezign.com>
2025-09-29 13:03:08 +0530

    implemented sso login api

--- src/services/axios/apiEndpoint.jsx
+++ src/services/axios/apiEndpoint.jsx
@@ -1,4 +1,5 @@
 const endPoint ={
-    ALL_QUESTIONS:"/narrative/all-questions"
+    ALL_QUESTIONS:"/narrative/all-questions",
+    Login : "/api/auth/check-token"
 }
 export default  endPoint;
--- src/services/axios/axiosInstance.jsx
+++ src/services/axios/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL || "http://192.168.10.11:3001",
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL || "http://192.168.0.78:3009",
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,14 +12,10 @@ const axiosInstance = axios.create({
 // Request Interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
- 
-    const token = "FOkB/AUT2wGXRv ablqvbg==";
-    const Auth = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndpbHByb3BoeUBpbmZvcm1lZC5wcm8iLCJuYW1lIjoid2lscHJvcGh5IiwiZ3JvdXBJZCI6MywibG9naW5UeXBlIjoiZGlyZWN0IiwidXNlclRva2VuIjoiRk9rQi9BVVQyd0dYUnYgYWJscXZiZz09IiwicGFzc3dvcmRJZCI6MjE0NzU0MTMwMCwiaWF0IjoxNzU4Nzk0NDkzLCJleHAiOjE3NTg3OTgwOTN9.7w6CcqLIPOhfHrbi-48vhdAxD5uIgFg1WSMh2tmRmeM`
-    if (token) {
-      config.headers.token = token;
-    }
-     if (Auth) {
-      config.headers.Auth = Auth;
+  
+    const auth = localStorage.getItem("decrypted_token");
+     if (auth) {
+      config.headers.auth = auth;
     }
     return config;
   },
