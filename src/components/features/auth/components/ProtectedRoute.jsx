5fbc54d07b154afae737d1b43e8a515b95f82e24
unknown <shivam.chauhan@shinedezign.com>
2025-10-01 10:29:54 +0530

    updated routing

--- src/components/features/auth/components/ProtectedRoute.jsx
+++ src/components/features/auth/components/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { clearLocalStorage } from '../../../../helper/helper';
 
 const ProtectedRoute = () => {
   const isAuthenticated =  localStorage.getItem("decrypted_token") ? true : false;
+  if(!isAuthenticated){
+    clearLocalStorage()
+  }
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
