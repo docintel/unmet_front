de4ef86d40ff01108de87bb02c7132731681c7ab
Radhe2001 <radheshyamjha469@gmail.com>
2025-10-02 10:33:52 +0530

    dynamic data from api integrated on home route for age group

--- src/components/features/patientJourney/Landing/PatientJourneyLanding.jsx
+++ src/components/features/patientJourney/Landing/PatientJourneyLanding.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-import { Row } from 'react-bootstrap'
-import {Link} from 'react-router-dom' 
-import JourneySectionList from '../Sections/JourneySectionList'
-import JourneySectionDetail from '../Sections/JourneySectionDetail'
-
+import React, { useContext, useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import JourneySectionList from "../Sections/JourneySectionList";
+import JourneySectionDetail from "../Sections/JourneySectionDetail";
+import { ContentContext } from "../../../../context/ContentContext";
 
 const PatientJourneyLanding = () => {
+  const { filterAges, fetchAgeGroups } = useContext(ContentContext);
   const [activeSection, setActiveSection] = useState(null);
   // const [isHcpSelected, setIsHcpSelected] = useState(false);
 
+  useEffect(() => {
+    (async () => await fetchAgeGroups())();
+  });
   // const toggleUserType = () => setIsHcpSelected((prev) => !prev);
   const handleSectionClick = (section) => setActiveSection(section);
 
   return (
-    <div className="main-page"> 
+    <div className="main-page">
       <div className="custom-container">
         <Row>
           <section className="journey-box">
@@ -22,31 +26,37 @@ const PatientJourneyLanding = () => {
                 <div className="explore-journey">
                   <h5>Explore your patient's Journey</h5>
                   <div className="patient-journey d-flex align-items-center justify-content-between">
-                    {[
-                      <span>
-                        Childhood
-                        <br />
-                        Age 0-5
-                      </span>,
-                       <span>Childhood<br />Age 6-11</span>,
-                       <span>Teen years<br />Age 12-17</span>,
-                      <span>Early adulthood<br />Age 18-25</span>,
-                      <span>Adulthood<br />Age 26-60</span>,
-                      <span>Older age<br />Age 60+</span>,
-                    ].map((label, idx) => (
-                      <React.Fragment key={idx}>
-                        <div className="journey-link">{label}</div>
-                        {idx !== 5 && <div className="line"></div>}
-                      </React.Fragment>
-                    ))}
+                    {filterAges &&
+                      filterAges.map((age, idx) => (
+                        <React.Fragment key={age.id}>
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: age.label,
+                            }}
+                            className="journey-link"
+                          ></div>
+                          {}
+                          {idx !== filterAges.length - 1 && (
+                            <div className="line"></div>
+                          )}
+                        </React.Fragment>
+                      ))}
                   </div>
-                  <Link to="/touchpoints" className="start-btn">Start</Link>
+                  <Link to="/touchpoints" className="start-btn">
+                    Start
+                  </Link>
                   {/* <Button variant="primary">Start</Button> */}
                 </div>
-                <JourneySectionList onSectionClick={handleSectionClick} section={activeSection} />
+                <JourneySectionList
+                  onSectionClick={handleSectionClick}
+                  section={activeSection}
+                />
               </>
             ) : (
-              <JourneySectionDetail onSectionClick={handleSectionClick} section={activeSection} />
+              <JourneySectionDetail
+                onSectionClick={handleSectionClick}
+                section={activeSection}
+              />
             )}
           </section>
         </Row>
