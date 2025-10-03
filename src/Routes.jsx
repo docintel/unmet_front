2e62d03276c994e2b473da6b6825aec7f97e64fd
unknown <shivam.chauhan@shinedezign.com>
2025-10-03 09:48:25 +0530

    added the public middleware

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,22 @@
-import React, { lazy } from "react";
+import React from "react";
 import { createBrowserRouter } from "react-router-dom"; 
 import ProtectedRoute from "./components/features/auth/components/ProtectedRoute"; 
+import PublicRoute from "./components/features/auth/components/PublicRoute.jsx";
 import Layout from "./components/features/patientJourney/Layout/Layout"; 
 import { privateRoutes, publicRoutes } from "./Routes/Routes.jsx";
 
 const Routing = createBrowserRouter([
-  ...publicRoutes, 
+  {
+    element: <PublicRoute />,
+    children: publicRoutes,
+  },
   {
     element: <ProtectedRoute />,
     children: [
-      { element: <Layout />, children: privateRoutes }
+      {
+        element: <Layout />,
+        children: privateRoutes,
+      }
     ]
   },
   { path: "*", element: <div>404 Not Found</div> }
