e3378b5aaee5251fdc800c26dae661482a7fe13f
Nishantk001 <nishant.kumar@shinedezign.com>
2025-10-13 12:38:45 +0530

    fix: responsiveness header & button

--- src/components/features/patientJourney/Pages/TouchPoints.jsx
+++ src/components/features/patientJourney/Pages/TouchPoints.jsx
@@ -256,8 +256,8 @@ const TouchPoints = () => {
     if (e) e.preventDefault();
     if (searchText.length >= 3 || searchText.length === 0) filterContents();
     else setToast({
-      type: "info",
-      title: "Warning",
+      type: "danger",
+      title: "Error",
       message: "Please enter at least three characters to search",
       show: true
     });
