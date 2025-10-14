cbe7a19db9fbf0534a75dfecd7a6a4ec5051951e
varunshine <varun.verma@shinedezign.com>
2025-10-14 18:41:15 +0530

    update the code for redirection after sso.

--- src/hooks/authConfig.jsx
+++ src/hooks/authConfig.jsx
@@ -2,8 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: import.meta.env.VITE_APP_MS_CLIENT_ID,
     authority: import.meta.env.VITE_APP_MS_AUTHORITY,
-    redirectUri: window.location.origin + "/",
-    postLogoutRedirectUri: window.location.origin + "/"
+    redirectUri: window.location.origin + "/login",
+    postLogoutRedirectUri: window.location.origin + "/login"
   },
   cache: {
     cacheLocation: "localStorage",
