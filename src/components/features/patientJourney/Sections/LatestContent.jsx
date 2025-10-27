f264f00c6ad9b50ed1a0743da31eab937baa2586
Radhe-2001 <radhey.shyam@shinedezign.com>
2025-10-27 10:14:17 +0530

    fixed the issue in the faq and latest content

--- src/components/features/patientJourney/Sections/LatestContent.jsx
+++ src/components/features/patientJourney/Sections/LatestContent.jsx
@@ -7,8 +7,8 @@ const LatestContent = () => {
   const [latestContent, setLatestContent] = useState([]);
 
   useEffect(() => {
-    if (content.length === 0) return;
-    const sortedData = content.sort((a, b) => {
+    if (content.loading || content.error) return;
+    const sortedData = content.data.sort((a, b) => {
       const dateA = new Date(a.creation_date.split(".").reverse().join("-")); // "25.September.2025" -> "2025-September-25"
       const dateB = new Date(b.creation_date.split(".").reverse().join("-"));
       return dateB - dateA; // latest first
