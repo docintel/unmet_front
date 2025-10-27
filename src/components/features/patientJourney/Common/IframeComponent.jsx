b170fa549e4405359bed7c57ddaa94b0027cc1fb
Radhe-2001 <radhey.shyam@shinedezign.com>
2025-10-27 17:02:08 +0530

    modified the preview content url in the iframe

--- src/components/features/patientJourney/Common/IframeComponent.jsx
+++ src/components/features/patientJourney/Common/IframeComponent.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContentContext } from "../../../../context/ContentContext";
 
 const IframeComponent = ({ previewArticle /* setCurrentReadClick */ }) => {
-  if (!previewArticle) return null; // Don't render if no URL
+  if (!previewArticle) return null;
+
+  const { isHcp } = useContext(ContentContext);
 
   return (
     <div className="w-100">
       <iframe
-        src={previewArticle}
+        src={previewArticle + (isHcp ? "_codeHcp" : "_codeOcta")}
         title="Embedded Content"
         width="100%"
         height="100%"
