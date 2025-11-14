6e06ac5937ed2fd8982a5f5565c96ee890e2d49d
Nishantk001 <nishant.kumar@shinedezign.com>
2025-11-14 17:27:46 +0530

    fix: Accont page question card ui

--- src/components/features/patientJourney/Common/IframeComponent.jsx
+++ src/components/features/patientJourney/Common/IframeComponent.jsx
@@ -1,21 +1,24 @@
 import React, { useContext } from "react";
 import { ContentContext } from "../../../../context/ContentContext";
 
-const IframeComponent = ({ previewArticle /* setCurrentReadClick */ }) => {
+const IframeComponent = ({ previewArticle /* setCurrentReadClick */ }) =>
+{
   if (!previewArticle) return null;
 
   const { isHcp } = useContext(ContentContext);
+  console.log(previewArticle, "previewArticle")
 
   return (
     <div className="w-100">
       <iframe
-        src={previewArticle + (isHcp ? "_codeHcp" : "_codeOcta")}
+        src={previewArticle.replace("https://docintel.app", "http://localhost:4200") + (isHcp ? "_codeHcp" : "_codeOcta")}
+        // src={previewArticle + (isHcp ? "_codeHcp" : "_codeOcta")}
         title="Embedded Content"
         width="100%"
         height="100%"
         style={{
           border: "none",
-          minHeight: "85vh",
+          minHeight: "100vh",
         }}
       ></iframe>
     </div>
