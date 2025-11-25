681ae231f647a823c20d4327d8fe27597f760362
unknown <shivam.chauhan@shinedezign.com>
2025-11-25 17:49:05 +0530

    updated the iframe issue

--- src/components/features/patientJourney/Common/IframeComponent.jsx
+++ src/components/features/patientJourney/Common/IframeComponent.jsx
@@ -6,13 +6,11 @@ const IframeComponent = ({ previewArticle /* setCurrentReadClick */ }) =>
   if (!previewArticle) return null;
 
   const { isHcp } = useContext(ContentContext);
-  console.log(previewArticle, "previewArticle")
 
   return (
     <div className="w-100">
       <iframe
-        src={previewArticle.replace("https://docintel.app", "http://localhost:4200") + (isHcp ? "_codeHcp" : "_codeOcta")}
-        // src={previewArticle + (isHcp ? "_codeHcp" : "_codeOcta")}
+        src={previewArticle + (isHcp ? "_codeHcp" : "_codeOcta")}
         title="Embedded Content"
         width="100%"
         height="100%"
